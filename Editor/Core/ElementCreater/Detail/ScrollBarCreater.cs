ba84c09cd72953ddf0295237360e69aad77d165f
zouhunter <1063627025@qq.com>
2018-01-20 23:14:33 +0800

    update ScrollBarCreater.cs

--- Editor/Core/ElementCreater/Detail/ScrollBarCreater.cs
+++ Editor/Core/ElementCreater/Detail/ScrollBarCreater.cs
@@ -11,24 +11,69 @@ using UnityEngine.Assertions.Comparers;
 using System.Collections;
 using System.Collections.Generic;
 using System;
+using UnityEditor;
 
 namespace CommonWidget
 {
-    public class ScrollBarCreater : ElementCreater
+    public class ScrollbarCreater : ElementCreater
     {
         public override GameObject CreateInstence(WidgetItem info)
         {
-            throw new NotImplementedException();
+            var ok = EditorApplication.ExecuteMenuItem("GameObject/UI/Scrollbar");
+            if (ok)
+            {
+                var obj = Selection.activeGameObject;
+                var bar = obj.GetComponent<Scrollbar>();
+                var dic = info.spriteDic;
+
+                if (dic.ContainsKey(KeyWord.background))
+                {
+                    var image = bar.GetComponent<Image>();
+                    image.sprite = dic[KeyWord.background];
+                    image.type = Image.Type.Simple;
+                    if (image.sprite.rect.width > image.sprite.rect.height)
+                    {
+                        bar.SetDirection(Scrollbar.Direction.LeftToRight, true);
+                    }
+                    else
+                    {
+                        bar.SetDirection(Scrollbar.Direction.BottomToTop, true);
+                    }
+                    image.SetNativeSize();
+                }
+
+                if (dic.ContainsKey(KeyWord.handle))
+                {
+                    bar.handleRect.GetComponent<Image>().sprite = dic[KeyWord.handle];
+                }
+
+                return obj;
+            }
+            return null;
         }
 
         public override List<Sprite> GetPreviewList(WidgetItem info)
         {
-            throw new NotImplementedException();
+            var list = new List<Sprite>();
+            var dic = info.spriteDic;
+            if (dic != null)
+            {
+                if (dic.ContainsKey(KeyWord.background))
+                {
+                    list.Add(dic[KeyWord.background]);
+                }
+
+                else if (dic.ContainsKey(KeyWord.handle))
+                {
+                    list.Add(dic[KeyWord.handle]);
+                }
+            }
+            return list;
         }
 
         protected override List<string> CreateDefultList()
         {
-            throw new NotImplementedException();
+            return new List<string>() { KeyWord.background,KeyWord.handle };
         }
     }
 }
