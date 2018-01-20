78c057c7de2510820d08852c90019ee2c816d3f8
zouhunter <1063627025@qq.com>
2018-01-20 22:52:32 +0800

    update cercleSliderCreater

--- Editor/Core/ElementCreater/Detail/CercalSliderCreater.cs
+++ Editor/Core/ElementCreater/Detail/CercalSliderCreater.cs
@@ -11,6 +11,7 @@ using UnityEngine.Assertions.Comparers;
 using System.Collections;
 using System.Collections.Generic;
 using System;
+using UnityEditor;
 
 namespace CommonWidget
 {
@@ -18,17 +19,57 @@ namespace CommonWidget
     {
         public override GameObject CreateInstence(WidgetItem info)
         {
-            throw new NotImplementedException();
+            var ok = EditorApplication.ExecuteMenuItem("GameObject/UI/Image");
+            if(ok)
+            {
+                var backgroundImage = Selection.activeGameObject;
+                if(info.spriteDic.ContainsKey(KeyWord.background))
+                {
+                    WidgetUtility.InitImage( backgroundImage.GetComponent<Image>(), info.spriteDic[KeyWord.background]);
+                }
+                ok = EditorApplication.ExecuteMenuItem("GameObject/UI/Image");
+                if(ok)
+                {
+                    var fillImage = Selection.activeGameObject.GetComponent<Image>();
+                    fillImage.name = KeyWord.fill;
+                  
+                    fillImage.type = Image.Type.Filled;
+                    fillImage.fillMethod = Image.FillMethod.Radial360;
+                    if(info.spriteDic.ContainsKey(KeyWord.fill))
+                    {
+                        fillImage.sprite = info.spriteDic[KeyWord.fill];
+                    }
+
+                    fillImage.transform.SetParent(backgroundImage.transform, false);
+                    (fillImage.transform as RectTransform).anchorMin = Vector2.zero;
+                    (fillImage.transform as RectTransform).anchorMax = Vector2.one;
+                    fillImage.transform.localPosition = Vector3.zero;
+                    (fillImage.transform as RectTransform).SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, (backgroundImage.transform as RectTransform).sizeDelta.x);
+                    (fillImage.transform as RectTransform).SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, (backgroundImage.transform as RectTransform).sizeDelta.y);
+
+                }
+                return backgroundImage;
+            }
+            return null;
         }
 
         public override List<Sprite> GetPreviewList(WidgetItem info)
         {
-            throw new NotImplementedException();
+            var list = new List<Sprite>();
+            if(info.spriteDic.ContainsKey(KeyWord.fill))
+            {
+                list.Add(info.spriteDic[KeyWord.fill]);
+            }
+            else if(info.spriteDic.ContainsKey(KeyWord.background))
+            {
+                list.Add(info.spriteDic[KeyWord.background]);
+            }
+            return list;
         }
 
         protected override List<string> CreateDefultList()
         {
-            throw new NotImplementedException();
+            return new List<string>() { KeyWord.background, KeyWord.fill };
         }
     }
 }
